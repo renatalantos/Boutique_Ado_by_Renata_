0bebe67f0327addf75edb4a5529c238cca68319e
renatalantos <renatalantos@gmail.com>
2021-11-24 15:19:27 +0000

    Set up secret key

--- boutiqe_ado/settings.py
+++ boutiqe_ado/settings.py
@@ -17,10 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+-8p3u)9y#hslm39sc1m352p^&@le_uga7xyfv$(ea$%wgau!^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-renatal.herokuapp.com', 'localhost']
 
