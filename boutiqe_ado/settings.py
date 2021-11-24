035e2b01325294261ebbf810dc37948c68edb020
renatalantos <renatalantos@gmail.com>
2021-11-24 15:00:45 +0000

    Test

--- boutiqe_ado/settings.py
+++ boutiqe_ado/settings.py
@@ -118,12 +118,11 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-
-else:    
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
